5c6f2a52566fa450ef071626260d702d57961d66
Roberto Sanson <rsanson@mobilize.net>
2022-11-09 14:08:17 -0500

    setup

--- feature-5/src/App.js
+++ feature-5/src/App.js
@@ -6,28 +6,9 @@ import Components from "./Components/Components.js";
 
 
 Parse.initialize(Env.SERVER_URL,Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
-// Parse.serverURL(Env.SERVER_URL);
 
 function App() {
   return <Components />;
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
