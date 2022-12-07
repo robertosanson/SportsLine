1cdc47d081fd8b5fd5462151f66b599404a9c1a2
Andres Lacayo <Andres@Andress-MacBook-Pro.local>
2022-12-07 06:28:02 -0500

    Added Stripe support

--- feature-5/src/Components/Buy/Child.js
+++ feature-5/src/Components/Buy/Child.js
@@ -3,7 +3,7 @@ import React from "react";
 // This page is simply creating a button that could be used for multiple pages.
 export default function Child({ data, onChildClick }) {
   return (
-    <div class="child">
+    <div className="child">
       <button onClick={onChildClick}>{data}</button>
     </div>
   );
--- feature-5/src/Components/Main/MainForm.js
+++ feature-5/src/Components/Main/MainForm.js
@@ -1,21 +1,45 @@
 import React from "react";
 import "../../styles.css";
-// import { loadStripe } from "@stripe/stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 // function that displays the items that are availble, as well as the
 // Vendors that correspond to each item
 const MainForm = ({ items }) => {
 
+  let stripePromise;
+  const getStripe = () => {
+    if (!stripePromise) {
+      stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
+    }
+    return stripePromise;
+  };
+
+  const redirectToCheckout = async (priceId) => {
+    console.log("redirectToCheckout");
+    const item = {
+      price: priceId,
+      quantity: 1
+    };
+    let checkoutOptions = {
+      lineItems: [item],
+      mode: "payment",
+      successUrl: `${window.location.origin}/success`,
+      cancelUrl: `${window.location.origin}/cancel`
+      };
+    const stripe = await getStripe();
+    const { error } = await stripe.redirectToCheckout(checkoutOptions);
+    console.log("Stripe checkout error", error);
+  };
+
   //This are the list items that will be presented to the user
   let listItems = items.map((item) => (
     
-    <div class="container">
-      <div class="panel-group">
-        <div class="panel panel-default">
+    <div className="container">
+      <div className="panel-group">
+        <div className="panel panel-default">
           <div key={item.get("name").toString()} className="item">
             {/* get image of the item */}
             <img
-              className="images"
               src={window.location.origin + "/pictures/items/" + item.get("picture")}
               alt={item.name}
               className="img-thumbnail"
@@ -26,7 +50,7 @@ const MainForm = ({ items }) => {
             Stock: {item.get("quantity")}
             <br />
             <br />
-            {/* <button onClick={redirectToCheckout(newItem)}>Buy</button> */}
+            <button onClick={() => {redirectToCheckout(item.get("stripeId"))}}>Buy</button>
           </div>
         </div>
       </div>
